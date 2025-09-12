690d42a446dee57d616b16903d0381f7ebce58dd
chaitanya <chaitanya@underlineit.co.in>
2025-09-12 16:14:26 +0530

    fix: added callback url

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,10 @@ export default function Home() {
   const { data: session } = useSession();
   const onSocialLogin = async () => {
     try {
-      const result: any = await signIn("apple");
+      const result: any = await signIn("apple", {
+        callbackUrl: "https://apple-auth-gamma.vercel.app/social-login",
+        redirect: false,
+      });
       if (get(result, "error")) {
       }
     } catch (error) {
