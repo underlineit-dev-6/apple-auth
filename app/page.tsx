1d46d844ecaa9b73fd66a727acf582afd9414198
chaitanya <chaitanya@underlineit.co.in>
2025-09-23 14:42:10 +0530

    feat: added logs

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 import { getSession, signIn, useSession } from "next-auth/react";
-import { get } from "lodash";
+import { get, set } from "lodash";
 import { FaApple } from "react-icons/fa";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
   const { data: session } = useSession();
   const navigate = useRouter();
+  const [login, setLogin] = useState(false);
+
   const onSocialLogin = async () => {
+    setLogin(true);
     try {
       const result: any = await signIn("apple", { redirect: false });
       const session = await getSession();
@@ -19,7 +23,7 @@ export default function Home() {
       console.log(error);
     }
   };
-  console.log(session);
+  console.log(login, session, "login session");
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <button
