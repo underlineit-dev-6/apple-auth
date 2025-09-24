49eaedf74c600e8dad0031a36f7cebbbf27259af
chaitanya <chaitanya@underlineit.co.in>
2025-09-24 13:09:19 +0530

    feat: added signout button

--- app/social-login/page.tsx
+++ app/social-login/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 import { get } from "lodash";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 function SocialLogin() {
   const { data: session } = useSession();
   return (
-    <h1>
-      Welcome {get(session, "user.email")} - {get(session, "user.provider","")}
-    </h1>
+    <div className="flex flex-col justify-center items-center h-screen gap-3">
+      <div>
+        <h1>
+          Welcome {get(session, "user.email")} -{" "}
+          {get(session, "user.provider", "")}
+        </h1>
+      </div>
+      <button onClick={async () => await signOut()}>Logout</button>
+    </div>
   );
 }
 export default SocialLogin;
